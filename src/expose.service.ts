aac8153b3d9ce6fb42fd5a469c0dad2f0f4b15ab
lanbery <cwd-pc@xy-info.com>
2025-03-27 16:13:19 +0800

    feat: updagte

--- src/expose.service.ts
+++ src/expose.service.ts
@@ -1,12 +1,16 @@
 import { Provider } from '@nestjs/common';
 
 import {
+  KnbaseService,
+  KnowledgeResourceService,
   PromptTemplateService,
   PromptOptionService,
   ModelProviderService,
 } from './services';
 
 export const shareServices: Provider[] = [
+  KnbaseService,
+  KnowledgeResourceService,
   ModelProviderService,
   PromptTemplateService,
   PromptOptionService,
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
+export * from './kn.base.service';
+export * from './kn.resource.service';
 export * from './model.provider.service';
 export * from './prompt.option.service';
 export * from './prompt.template.service';
